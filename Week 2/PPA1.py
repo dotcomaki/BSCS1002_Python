e947626e0204feec351e9942d56d2f330a693b16
Aki <114000157+dotcomaki@users.noreply.github.com>
2024-02-04 15:56:56 +0530

    Create PPA1.py

---
+++ Week 2/PPA1.py
@@ -0,0 +1,9 @@
+'''
+Print positive if it is greater than zero and negative if it is less than zero. You can assume that the input will be non-zero.
+'''
+
+x = int(input())
+if x > 0:
+    print("positive")
+elif x < 0:
+    print("negative")
