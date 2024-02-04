3d404d5ddd389117050256b4d5edf9120c5ec7ef
Aki <114000157+dotcomaki@users.noreply.github.com>
2024-02-04 15:48:29 +0530

    Create PPA8.py

---
+++ Week 1/PPA8.py
@@ -0,0 +1,13 @@
+'''
+Print the following pattern. There is exactly one space between any two consecutive numbers on any line. There are no spaces at the end of any line.
+1 2 1
+1 2 3 2 1
+1 2 3 4 3 2 1
+1 2 3 4 5 4 3 2 1
+'''
+
+print(1,2,1)
+print(1,2,3,2,1)
+print(1,2,3,4,3,2,1)
+print(1,2,3,4,5,4,3,2,1)
+#It told me not to use iteration
