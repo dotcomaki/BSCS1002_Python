5a23a3dbc27be1c1a49a4b8347469149d9ead72c
Aki <114000157+dotcomaki@users.noreply.github.com>
2024-02-04 15:47:52 +0530

    Create PPA7.py

---
+++ Week 1/PPA7.py
@@ -0,0 +1,11 @@
+'''
+Accept a five digit number as input and print the sum of its digits as output.
+'''
+
+x = input()
+first = int(x[0])
+second = int(x[1])
+third = int(x[2])
+fourth = int(x[3])
+fifth = int(x[4])
+print(first + second + third + fourth + fifth)
