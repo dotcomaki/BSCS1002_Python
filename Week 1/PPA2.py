9b20e1c1afbc955653052fffc792a9e260cdf8bb
Aki <114000157+dotcomaki@users.noreply.github.com>
2024-02-04 15:45:24 +0530

    Create PPA2.py

---
+++ Week 1/PPA2.py
@@ -0,0 +1,16 @@
+'''
+Print the following pattern.
+*
+**
+***
+****
+*****
+There are no spaces between consecutive stars. There are no spaces at the end of each line.
+'''
+
+print('*')
+print('**')
+print('***')
+print('****')
+print('*****')
+#It told me not to use iteration
