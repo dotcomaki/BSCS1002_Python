4234482a34bb3a51b1f0e49d76b6129a94417d61
Aki <114000157+dotcomaki@users.noreply.github.com>
2024-02-04 18:41:06 +0530

    Create PPA7.py

---
+++ Week 2/PPA7.py
@@ -0,0 +1,17 @@
+'''
+Question's too long
+'''
+
+a = input()
+b = input()
+c = input()
+d = input()
+e = input()
+
+if ((a in b)
+        and (b in c)
+        and (c in d)
+        and (d in e)):
+    print("magical")
+else:
+    print("non-magical")
