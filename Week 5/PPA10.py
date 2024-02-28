448c693897d24248a498c10655f09ef93d904cd9
Akif <dotcomakif@gmail.com>
2024-02-28 20:15:18 +0530

    Update PPA10.py

--- Week 5/PPA10.py
+++ Week 5/PPA10.py
@@ -1,5 +1,6 @@
 '''
-Write a function insert that accepts a sorted list L of integers and an integer x as input. The function should return a sorted list with the element x inserted at the right place in the input list. The original list should not be disturbed in the process.
+Write a function insert that accepts a sorted list L of integers and an integer x as input. The function should return a sorted list with the element x inserted at the right place in the input list.
+The original list should not be disturbed in the process.
 '''
 
 def insert(L, x):
