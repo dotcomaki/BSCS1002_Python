495ddce55a51e4a5661726b19425724d98486e09
Akif <dotcomakif@gmail.com>
2024-03-02 17:54:26 +0530

    Create PPA4.py

---
+++ Week 6/PPA4.py
@@ -0,0 +1,5 @@
+'''
+Write a function named value_to_keys that accepts a dictionary D and a variable named value as arguments
+It should return the list of all keys in the dictionary that have value equal to value. If the value is not present in the dictionary, the function should return the empty list.
+'''
+
