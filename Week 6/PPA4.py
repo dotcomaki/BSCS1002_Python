9475760f59f6f53dbff3c66adbf9990208ed0661
Akif <dotcomakif@gmail.com>
2024-03-02 17:56:40 +0530

    Update PPA4.py

--- Week 6/PPA4.py
+++ Week 6/PPA4.py
@@ -3,3 +3,9 @@ Write a function named value_to_keys that accepts a dictionary D and a variable 
 It should return the list of all keys in the dictionary that have value equal to value. If the value is not present in the dictionary, the function should return the empty list.
 '''
 
+def value_to_keys(D, value):
+    l = []
+    for key in D:
+        if D[key] == value:
+            l.append(key)
+    return l
