68d9cdd1135af1f98b343f2475363b263c02cff4
Akif <dotcomakif@gmail.com>
2024-03-02 19:45:36 +0530

    Update PPA8.py

--- Week 6/PPA8.py
+++ Week 6/PPA8.py
@@ -6,3 +6,29 @@ Write the following functions
 The value corresponding to a key, is the set of all factors of key. This function must make use of factors.
 '''
 
+def factors(n):
+    l = set()
+    for i in range(1, n + 1):
+        if n % i == 0:
+            l.add(i)
+    return l
+
+
+def common_factors(a,b):
+    s = set()
+    if a > b: max = a
+    else: max = b
+    for i in range(1, max + 1):
+        if a % i == 0 and b % i == 0:
+            s.add(i)
+    return s
+
+def factors_upto(n):
+    D = {}
+    for i in range(1, n + 1):
+        s = set()
+        for j in range(1, i + 1):
+            if i % j == 0:
+                s.add(j)
+        D[i] = s
+    return D
