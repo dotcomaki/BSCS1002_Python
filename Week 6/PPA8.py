7ab9b509635603a25027b9c239b8a0566add0c83
Akif <dotcomakif@gmail.com>
2024-03-02 18:21:27 +0530

    Create PPA8.py

---
+++ Week 6/PPA8.py
@@ -0,0 +1,8 @@
+'''
+Write the following functions
+(1) factors: accept a positive integer n as argument. Return the set of all factors of n.
+(2) common_factors: accept two positive integers a and b as arguments. Return the set of common factors of the two numbers. This function must make use of factors.
+(3) factors_upto: accept a positive integer n as argument. Return a dict D, whose keys are integers and values are sets. Each integer in the range [1,n], endpoints inclusive, is a key of D.
+The value corresponding to a key, is the set of all factors of key. This function must make use of factors.
+'''
+
