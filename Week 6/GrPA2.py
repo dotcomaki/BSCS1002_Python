52c0ae9ba70ff56d0f18d7727e7ce94d53c1c47c
Akif <dotcomakif@gmail.com>
2024-03-04 14:12:46 +0530

    Create GrPA2.py

---
+++ Week 6/GrPA2.py
@@ -0,0 +1,17 @@
+'''
+Question's too long
+'''
+
+def freq_to_words(words):
+    word_set = set(words)
+    d = {}
+    for i in word_set:
+        count = 0
+        for j in range(len(words)):
+            if words[j] == i:
+                count = count + 1
+        if count in d:
+            d[count].append(i)
+        else:
+            d[count]=[i]
+    return d
