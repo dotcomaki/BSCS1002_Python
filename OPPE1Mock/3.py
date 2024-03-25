78d3aeb967572f0ca021ee7e05eaae65fd16ca40
Akif M <dotcomakif@gmail.com>
2024-03-25 14:25:12 +0530

    Delete OPPE1Mock directory

--- OPPE1Mock/3.py
+++
@@ -1,7 +0,0 @@
-'''
-Accept the date in MM/DD/YYYY format as input from the user and print the date in DD-MM-YY format, by retaining only the last two digits in a year, replacing the forward slash with a dash and swapping the order of month and date.
-'''
-
-date = input()
-newdate = date[3:5] + '-' + date[0:2] + '-' + date[8:]
-print(newdate)
